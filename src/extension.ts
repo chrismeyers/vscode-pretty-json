74f76a16dd7402405d4a47a8df245f56c4a8b8e9
Chris Meyers <chris@chrismeyers.net>
2021-08-03 17:18:10 -0600

    Added basic functionality
    Running the command for the first time only changes the document
    language and requires the command to be run again. After that both steps
    work on the first run.

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,28 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-pretty-json" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'vscode-pretty-json.prettyjson',
+    async () => {
+      if (vscode.window.activeTextEditor) {
+        vscode.languages
+          .setTextDocumentLanguage(
+            vscode.window.activeTextEditor.document,
+            'json'
+          )
+          .then((document) => {
+            vscode.commands.executeCommand('editor.action.formatDocument');
+          });
+      } else {
+        vscode.window.showErrorMessage('Pretty JSON: No active document!');
+      }
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-pretty-json.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-pretty-json!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
